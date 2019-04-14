a0356473ad0d690feff3b5989f630b3b1eefcfe3
new bee <jobas2007@gmail.com>
2019-04-13 22:07:47 -0400

    Refactored app config

--- src/main/java/com/example/demo/config/AppConnectionConfig.java
+++ src/main/java/com/example/demo/config/AppConnectionConfig.java
@@ -1,19 +1,20 @@
 package com.example.demo.config;
 
-import java.net.URISyntaxException;
-
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
@@ -21,8 +22,12 @@ import org.springframework.transaction.PlatformTransactionManager;
  *
  */
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = { "com.example.demo.repository" })
 public class AppConnectionConfig {
+
+	@Autowired
+	JpaVendorAdapter jpaVendorAdapter;
 
 	@Bean(name = "appDataSource")
 	@ConfigurationProperties("spring.app.datasource")
@@ -30,23 +35,23 @@ public class AppConnectionConfig {
 		return DataSourceBuilder.create().build();
 	}
 
-	@Bean(name ="entityManagerFactory")
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
+	public EntityManager entityManager() {
+		return entityManagerFactory().createEntityManager();
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
 		entityManagerFactoryBean.setDataSource(dataSource());
 		entityManagerFactoryBean.setPackagesToScan("com.example.demo.model");
-		
-		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
 		entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-		
-		return entityManagerFactoryBean;
+		entityManagerFactoryBean.afterPropertiesSet();
+		return entityManagerFactoryBean.getObject();
 	}
 
-	@Bean(name = "transactionManager")
-	public PlatformTransactionManager jpaTransactionManager(EntityManagerFactory entityManagerFactory)
-			throws URISyntaxException {
-		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(entityManagerFactory);
-		return transactionManager;
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+
+		return new JpaTransactionManager(entityManagerFactory());
 	}
 }
